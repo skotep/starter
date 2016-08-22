9b24da8105cf34997756678885f707ad27e8f707
skotep <skotep@gmail.com>
2016-08-21 19:19:17 -0500

    update for live bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,25 @@ module.exports = {
         }, {
             test: /\.json$/,
             loader: 'json'
-        }]
-    }
+        }, {
+          test: /\.css$/,
+          loader: 'style!css?sourceMap'
+        }, {
+          test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url?limit=10000&mimetype=application/font-woff"
+        }, {
+          test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url?limit=10000&mimetype=application/font-woff"
+        }, {
+          test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url?limit=10000&mimetype=application/octet-stream"
+        }, {
+          test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "file"
+        }, {
+          test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "url?limit=10000&mimetype=image/svg+xml"
+        }
+    ]
+  }
 }
