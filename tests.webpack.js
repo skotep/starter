2b897ebed353226f5e5f23c1d52c749caf81bf27
skotep <skotep@gmail.com>
2016-09-05 16:16:54 -0500

    fix tests.webpack

--- tests.webpack.js
+++ tests.webpack.js
@@ -1,4 +1,2 @@
 var context = require.context('./src', true, /^((?!index.js).)*\.jsx?$/);
-//var context = require.context('./src', true, /ttt.spec.js/);
-//var context = require.context('./src', true, /.*\.jsx?$/);
 context.keys().forEach(context);
